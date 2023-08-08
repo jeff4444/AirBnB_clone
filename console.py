342134c1eecfe518e9d93b17029d5c79e97ec67f
Jeff Mofor <allojeff444@gmail.com>
2023-08-08 04:00:51 -0700

    Implemented create in console

--- console.py
+++ console.py
@@ -4,7 +4,7 @@
 
 import cmd
 import sys
-
+import models
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -16,6 +16,18 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program"""
         sys.exit(0)
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel and saves it"""
+        if arg:
+            if arg == 'BaseModel':
+                my_instance = models.base_model.BaseModel()
+                my_instance.save()
+                print(my_instance.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
 
 if __name__ == '__main__':
