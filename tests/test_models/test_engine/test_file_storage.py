2b843cdcd385275dd43d4c64eb893f81e8cd3475
Jeff Mofor <allojeff444@gmail.com>
2023-08-13 13:52:03 -0700

    Added file storage test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import models
+"""Tests the base_model module"""
 
+storage = FileStorage()
+class TestBaseModel(unittest.TestCase):
+    """Test base_model class"""
+
+    def test_all(self):
+        self.assertEqual(models.storage.all(), {})
+        myModel = BaseModel()
+        objects = {}
+        objects[f'BaseModel.{myModel.id}'] = myModel
+        self.assertEqual(models.storage.all(), objects)
+        del models.storage.all()[f'BaseModel.{myModel.id}']
+        models.storage.save()
+
+    def test_new(self):
+        myModel = BaseModel()
+        self.assertEqual(myModel, models.storage.all()[f'BaseModel.{myModel.id}'])
+        del models.storage.all()[f'BaseModel.{myModel.id}']
+        models.storage.save()
+
+    def test_saveAndReload(self):
+        myModel = BaseModel()
+        store = FileStorage()
+        store.reload()
+        self.assertEqual(myModel, store.all()[f'BaseModel.{myModel.id}'])
+        del models.storage.all()[f'BaseModel.{myModel.id}']
+        models.storage.save()
+        self.assertEqual(models.storage.all(), {})
