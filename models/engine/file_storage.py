ac5b8f49f3f8fdda7fe041c7c32578ebb4e0868c
Jeff Mofor <allojeff444@gmail.com>
2023-08-13 07:30:01 -0700

    Edited file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,27 +21,27 @@ class FileStorage:
     def all(self):
         """Returns all objecrs"""
 
-        return self.__objects
+        return type(self).__objects
 
     def new(self, obj):
         """Adds a new object to our objects dict"""
 
-        self.__objects[f'{type(obj).__name__}.{obj.id}'] = obj
+        type(self).__objects[f'{type(obj).__name__}.{obj.id}'] = obj
 
     def save(self):
         """Save our object dict to our file path"""
 
         dict_to_save = {}
-        for key, val in self.__objects.items():
+        for key, val in type(self).__objects.items():
             dict_to_save[key] = val.to_dict()
-        with open(self.__file_path, 'w', encoding='utf-8') as f:
+        with open(type(self).__file_path, 'w', encoding='utf-8') as f:
             json.dump(dict_to_save, f)
 
     def reload(self):
         """Reloads objects from file path"""
 
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
+            with open(type(self).__file_path, 'r', encoding='utf-8') as f:
                 dict_loaded = json.load(f)
                 class_dicts = {'BaseModel': BaseModel,
                                'User': User,
@@ -54,6 +54,6 @@ class FileStorage:
                 for key1, val in dict_loaded.items():
                     for key in class_dicts:
                         if key1[0:len(key)] == key:
-                            self.__objects[key1] = class_dicts[key](**val)
+                            type(self).__objects[key1] = class_dicts[key](**val)
         except FileNotFoundError:
             return
